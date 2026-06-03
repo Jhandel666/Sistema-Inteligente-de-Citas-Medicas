4bcf29b542e60ebaf28a7c241497ebd0111d61f7
Jhandel666 <jhandeljesuschavezmiranda4@gmail.com>
2026-06-02 19:46:56 -0500

    Add Vercel domain to CORS

--- app/main.py
+++ app/main.py
@@ -27,6 +27,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
+        "https://sistema-inteligente-de-citas-medicas-a6q03efx3.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
